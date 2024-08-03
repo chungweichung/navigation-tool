ccc6ee95a70789d3c7aa8f410bcde349dd1f27df
weichung <bio891128@gmail.com>
2024-08-03 16:47:36 +0800

    remove some annotation

--- lib/src/position.dart
+++ lib/src/position.dart
@@ -1,5 +1,32 @@
 ///lat and long are the radian unit
 class Position {
-  double lat, long;
+  final double lat;
+  final double long;
+
   Position(this.lat, this.long);
+
+  Position.clone(Position other) : this(other.lat, other.long);
+
+  Position operator *(num factor) {
+    return Position(lat * factor, long * factor);
+  }
+
+  Position operator +(Position other) {
+    return Position(lat + other.lat, long + other.long);
+  }
+
+  Position operator -(Position other) {
+    return Position(lat - other.lat, long - other.long);
+  }
+
+  /*double distanceTo(Position other) {
+    double dlat = lat - other.lat;
+    double dlong = long - other.long;
+    return sqrt(dlat * dlat + dlong * dlong);
+  }*/
+
+  @override
+  String toString() => 'Position(lat: $lat, long: $long)';
 }
+
+// 为 num 类型（包括 int 和 double）添加扩展方法
